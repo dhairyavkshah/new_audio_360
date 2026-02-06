7f6a76462a7e001d837b086ff1bb1eb24d9fb7b9
shahdhairyavk <52010586-shahdhairyavk@users.noreply.replit.com>
2026-02-06 14:04:43 +0000

    Restored to '71760aca9ed033f849768a462c18bc1b9d9d0e4b'
    Replit-Restored-To: 71760aca9ed033f849768a462c18bc1b9d9d0e4b

--- app.config.js
+++ app.config.js
@@ -110,10 +110,6 @@ export default {
         {
           android: {
             newArchEnabled: false,
-            compileSdkVersion: 35,
-            targetSdkVersion: 35,
-            buildToolsVersion: '35.0.0',
-            ndkVersion: '27.1.12297006',
           },
           ios: {
             newArchEnabled: false,
